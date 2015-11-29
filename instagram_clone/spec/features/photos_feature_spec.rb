b7325a34bf59d3502c377c2a08883e378bc50d59
Fergus Lemon <fergus.lemon@gmail.com>
2015-11-29 11:52:31 +0000

    create sign up helper and create upload photo spec

--- instagram_clone/spec/features/photos_feature_spec.rb
+++ instagram_clone/spec/features/photos_feature_spec.rb
@@ -8,4 +8,19 @@ feature "photos" do
       expect(page).to have_link "Add a photo"
     end
   end
+
+  before do
+    sign_up(user)
+  end
+
+  context "creating photos" do
+    scenario "prompts user to fill in a form and then displays photos" do
+      visit '/'
+      click_link 'Add a photo'
+      fill_in 'Description', with: 'Summer holiday'
+      click_button 'Upload photo'
+      expect(current_path).to eq '/photos'
+      expect(page).to have_content 'Summer holiday'
+    end
+  end
 end
